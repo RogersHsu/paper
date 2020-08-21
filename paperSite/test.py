751df8797b6d2fa50b77978a1a0d7e0d90ae100b
RogersHsu <53262515+RogersHsu@users.noreply.github.com>
2020-08-21 10:33:17 +0800

    Delete test.py

--- paperSite/test.py
+++
@@ -1,6 +0,0 @@
-newList = []
-change_data=[[255.0, 255.0, 255.0, 255.0], [255.0, 255.0, 255.0, 255.0], [255.0, 255, 255.0, 255.0], [255.0, 255.0, 255.0, 255.0]]
-
-newList.append(change_data)
-newList.append(change_data)
-print(newList)
