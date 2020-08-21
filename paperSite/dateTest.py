3c92dab8a4221bfc4cc6f945b61041698d90cab3
RogersHsu <53262515+RogersHsu@users.noreply.github.com>
2020-08-21 10:34:59 +0800

    Delete dateTest.py

--- paperSite/dateTest.py
+++
@@ -1,5 +0,0 @@
-import datetime
-today = datetime.date.today()
-nextMonday = today + datetime.timedelta(days=-today.weekday(), weeks=1)
-thisMonday = today - datetime.timedelta(days=today.weekday())
-print(thisMonday)
